9607aff17899d51245f6d23e08458df0b3a3af55
victormarcos-dev <victormarcosgl@gmail.com>
2025-11-14 10:40:05 -0300

    Delete contador directory

--- contador/script.js
+++
@@ -1,7 +0,0 @@
-const textarea = document.getElementById('texto');
-const contador = document.getElementById('contador');
-
-textarea.addEventListener('input', () => {
-  const quantidade = textarea.value.length;
-  contador.textContent = `${quantidade} caracteres`;
-});
